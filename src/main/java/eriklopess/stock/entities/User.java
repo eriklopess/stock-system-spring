dd0f02a5ba11346ad608171c85f7f620568b4352
Erik Lopes <erik.lopesoliveira2019@gmail.com>
2022-10-11 17:52:42 -0300

    criando entidade User e consertando erros no teste da entidade.

---
+++ src/main/java/eriklopess/stock/entities/User.java
@@ -0,0 +1,28 @@
+package eriklopess.stock.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+class User implements Serializable {
+    @Id
+    private int id;
+
+    private String name;
+
+    private String password;
+
+    private int roleId;
+}
---
+++ src/test/java/eriklopess/stock/entities/UserEntityTest.java
@@ -0,0 +1,17 @@
+package eriklopess.stock.entities;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static  org.junit.jupiter.api.Assertions.*;
+
+public class UserEntityTest {
+
+    @Test
+    @DisplayName(value="Shoud return the User with all yours properties")
+    public void ShoudReturnTheUserWithAllYoursProperties() {
+        User user = new User(1, "Julio", "1234", 2);
+        assertEquals(1, user.getId());
+        assertEquals("Julio", user.getName());
+        assertEquals(2, user.getRoleId());
+    }
+}
